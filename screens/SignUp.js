30fa58ae8af28298edb5c694e143c46afa495059
Kaitlyn Martinez <kkmartinez95@gmail.com>
2019-11-22 14:03:05 -0500

    Looks more like an app now

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -24,12 +24,71 @@ import {
 class SignUp extends Component {
   constructor() {
     super();
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
+  signUp(email, password) {
+    try {
+      if (this.state.password < 6) {
+        alert('Please enter atleast 6 characters');
+        return;
+      }
+      email = this.state.email;
+      password = this.state.password;
+      // username = this.state.username;
+      // firebase.auth().createUserWithEmailAndPassword(email, password); **
+
+      // firebase.auth().currentUser.providerData[0].displayName = username;
+      // console.log("CURRENT USER", firebase.auth());
+
+      this.props.navigation.navigate('Dashboard');
+    } catch (error) {
+      console.log(error.toString());
+    }
+  }
+
   render() {
     return (
-      <View style={styles.center}>
-        <Text>SignUp Page</Text>
-      </View>
+      <ImageBackground
+        style={styles.title}
+        source={require('../public/City.jpg')}>
+        <Image
+          source={require('../public/WeOut.png')}
+          style={styles.MainLogo}
+        />
+        <View style={styles.centerish}>
+          <Item floatingLabel>
+            <Label> Email </Label>
+
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={email => this.setState({email: email})}
+            />
+          </Item>
+
+          <Item floatingLabel>
+            <Label> Password </Label>
+
+            <Input
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={password => this.setState({password: password})}
+            />
+          </Item>
+
+          <Button
+            style={styles.LogSignBtnCentered}
+            full
+            success
+            onPress={() => this.signUp(this.state.email, this.state.password)}>
+            <Text style={styles.BtnText}>Sign Up</Text>
+          </Button>
+        </View>
+      </ImageBackground>
     );
   }
 }
