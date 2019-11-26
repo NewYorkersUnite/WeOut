a2002f46f1b25f59ec8c0d6d0e24b43e0664992b
Vanessa Chan <vchan8084@gmail.com>
2019-11-26 12:14:31 -0500

    bottom nav on all pages

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,36 +1,44 @@
 import React, {Component} from 'react';
-import {ScrollView, Image, Text} from 'react-native';
+import {ScrollView, Image, Text, View} from 'react-native';
 import styles from '../public/styles';
 import {Button} from 'native-base';
 
-export default class Profile extends Component {
+export default class Dashboard extends Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <ScrollView>
+      <View style={styles.mainContainer}>
         <Text style={styles.center}>PROFILE!!</Text>
-        <Button
-          style={styles.LogSignBtn}
-          full
-          onPress={() => navigate('Dashboard')}>
-          <Text style={styles.BtnText}> Dash</Text>
-        </Button>
-        <Button
-          style={styles.LogSignBtn}
-          full
-          onPress={() => navigate('Search')}>
-          <Text style={styles.BtnText}> Search</Text>
-        </Button>
-        <Button style={styles.LogSignBtn} full onPress={() => navigate('Poll')}>
-          <Text style={styles.BtnText}> Poll</Text>
-        </Button>
-        <Button
-          style={styles.LogSignBtn}
-          full
-          onPress={() => navigate('Profile')}>
-          <Text style={styles.BtnText}> Profile</Text>
-        </Button>
-      </ScrollView>
+        <View style={styles.navContainer}>
+          <View style={styles.navButtonContainer}>
+            <Button
+              style={styles.NavButton}
+              onPress={() => navigate('Dashboard')}>
+              <Text style={styles.NavBtnText}> Dash</Text>
+            </Button>
+          </View>
+
+          <View style={styles.navButtonContainer}>
+            <Button style={styles.NavButton} onPress={() => navigate('Search')}>
+              <Text style={styles.NavBtnText}> Search</Text>
+            </Button>
+          </View>
+
+          <View style={styles.navButtonContainer}>
+            <Button style={styles.NavButton} onPress={() => navigate('Poll')}>
+              <Text style={styles.NavBtnText}> Poll</Text>
+            </Button>
+          </View>
+
+          <View style={styles.navButtonContainer}>
+            <Button
+              style={styles.NavButton}
+              onPress={() => navigate('Profile')}>
+              <Text style={styles.NavBtnText}> Profile</Text>
+            </Button>
+          </View>
+        </View>
+      </View>
     );
   }
 }
