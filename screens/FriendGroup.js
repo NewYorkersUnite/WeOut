8c0955476817d9a0ea1cf0734da695dd81e98744
Kaitlyn Martinez <kkmartinez95@gmail.com>
2019-11-29 14:11:33 -0500

    more stuff for profile UI

---
+++ screens/FriendGroup.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {ScrollView, Image, Text, View, ImageBackground} from 'react-native';
+import styles from '../public/styles';
+import {Button} from 'native-base';
+
+export default class FriendGroup extends Component {
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <ImageBackground
+        style={styles.title}
+        source={require('../public/Background.png')}>
+        <View style={styles.mainContainer}>
+          <Text style={styles.center}>FriendGroup!!</Text>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
