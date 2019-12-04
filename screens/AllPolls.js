a1770cd52f292c9a49bfcb7dadace5a0c80b3943
Vanessa Chan <vchan8084@gmail.com>
2019-12-04 18:17:11 -0500

    timer sort working

--- screens/AllPolls.js
+++ screens/AllPolls.js
@@ -32,46 +32,47 @@ class AllPolls extends Component {
         <View>
           <ScrollView>
             {this.props.allPolls.map(poll => {
-              return (
-                <TouchableOpacity
-                  onPress={() =>
-                    this.props.navigation.navigate('VotingRoom', {poll})
-                  }>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      width: 100,
-                      height: 100,
-                      alignItems: 'center',
-                    }}>
-                    <Image
-                      source={{
-                        uri:
-                          randomNYCphotos[
-                            Math.floor(Math.random() * randomNYCphotos.length)
-                          ],
-                      }}
+              const thisMoment = new Date();
+              const date = new Date(poll.endTime.seconds * 1000);
+              let minutes = Math.floor((date - thisMoment) / 1000 / 60);
+              // console.log('Minutes left is ', minutes);
+              if (minutes > 0) {
+                return (
+                  <TouchableOpacity
+                    onPress={() =>
+                      this.props.navigation.navigate('VotingRoom', {poll})
+                    }>
+                    <View
                       style={{
-                        width: 80,
-                        height: 80,
-                        borderRadius: 50,
-                        marginLeft: 10,
-                        marginTop: 10,
-                        marginRight: 10,
-                      }}
-                    />
-                    <View style={styles.pollDetails}>
-                      <Text style={styles.scrollTxt}>{poll.themeTitle}</Text>
-                      <Text>
-                        Suggestions Closing In: {poll.suggestionTimer} minutes
-                      </Text>
-                      <Text>
-                        Final Votes Closing In: {poll.voteTimer} minutes
-                      </Text>
+                        flexDirection: 'row',
+                        width: 100,
+                        height: 100,
+                        alignItems: 'center',
+                      }}>
+                      <Image
+                        source={{
+                          uri:
+                            randomNYCphotos[
+                              Math.floor(Math.random() * randomNYCphotos.length)
+                            ],
+                        }}
+                        style={{
+                          width: 80,
+                          height: 80,
+                          borderRadius: 50,
+                          marginLeft: 10,
+                          marginTop: 10,
+                          marginRight: 10,
+                        }}
+                      />
+                      <View style={styles.pollDetails}>
+                        <Text style={styles.scrollTxt}>{poll.themeTitle}</Text>
+                        <Text>Final Votes Closing In: {minutes} minutes</Text>
+                      </View>
                     </View>
-                  </View>
-                </TouchableOpacity>
-              );
+                  </TouchableOpacity>
+                );
+              }
             })}
           </ScrollView>
         </View>
